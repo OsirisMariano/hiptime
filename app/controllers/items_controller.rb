9b2eaf02a42968de85ea4e436dc76208be47482f
Osiris Mariano <oslimacontato@gmail.com>
2024-04-15 21:27:16 -0300

    feat: feature/66 Continues working on CRUD ability (Create and Read)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@
 
 class ItemsController < ApplicationController
   def index
-  
+    @item = Item.all.order("created_ad DESC")
   end
 
   def new
@@ -10,6 +10,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to(root_path)
+    else
+      render("new")
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description)
   end
 end
